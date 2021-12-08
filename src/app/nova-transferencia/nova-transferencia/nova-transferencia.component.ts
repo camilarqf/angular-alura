c1bcc64b1d0b083576af8ec0ab4b9f568237656e
Camila Rocha de Queiroz Fonseca <camila.fonseca@corregedoria.df>
2021-12-08 09:35:21 -0300

    Aula 02 - Formulário de nova transferência

--- src/app/nova-transferencia/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia/nova-transferencia.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.css']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  valor: number = 12;
+  destino: number = 2454;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  transferir() {
+    console.log('solicitada nova transferência');
+    console.log('valor:', this.valor);
+    console.log('destino:', this.destino);
   }
-
 }
